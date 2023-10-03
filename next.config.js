7f1a252590ec693af60f59f831aa1c0d138543d1
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2023-10-03 20:21:31 -0300

    fix: include mdx content in vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': ['./content/**/*'],
+    },
+  },
+}
 
 module.exports = nextConfig
